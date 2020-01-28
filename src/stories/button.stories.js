b6e60133451708112e00664b7ba7b37f3d2661bc
KaanDarcey <kaanhdarcey@gmail.com>
2020-01-28 15:26:58 -0800

    default button comp created

---
+++ src/stories/button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../comps/button';
+
+export default {
+    title: 'Button',
+    component: Button,
+  };
+
+  export const defaultButton = () => {
+    return <Button />
+  };
