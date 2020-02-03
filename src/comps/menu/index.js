93ad702032f24c72b729b1b93cea94b3f50ab07b
KaanDarcey <kaanhdarcey@gmail.com>
2020-02-03 09:52:52 -0800

    slight styling adjustments

--- src/comps/menu/index.js
+++ src/comps/menu/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './menu.scss';
-import MenuItem from './menuItem';
-import Button from '../button';
+import MenuItem from './MenuItem';
+import Button from '../Button';
 import {FaHome,FaCalendarAlt, FaChartBar, FaRegClock, FaRegUserCircle, FaWallet, FaPowerOff, FaUsers, FaProjectDiagram} from 'react-icons/fa';
 
 function Menu({logo}){
     return(
-        <div className='container'>
+        <div className='menu-container'>
             <img className='logo' src={logo} alt='logo'/>
-                    <MenuItem icon={<FaChartBar />} title='Dashboard'/>
-                    <MenuItem icon={<FaCalendarAlt />} title='Schedule'/>
-                    <MenuItem icon={<FaProjectDiagram />} title='Projects'/>
-                    <MenuItem icon={<FaUsers />} title='Clients'/>
-                    <MenuItem icon={<FaRegClock />} title='Timer'/>
-                    <MenuItem icon={<FaRegUserCircle />} title='Account'/>
-                    <MenuItem icon={<FaChartBar />} title='Billing'/>
 
+                <MenuItem icon={<FaChartBar />} title='Dashboard' />
+                <MenuItem icon={<FaCalendarAlt />} title='Schedule'/>
+                <MenuItem icon={<FaProjectDiagram />} title='Projects'/>
+                <MenuItem icon={<FaUsers />} title='Clients'/>
+                <MenuItem icon={<FaRegClock />} title='Timer'/>
+                <MenuItem icon={<FaRegUserCircle />} title='Account'/>
+                <MenuItem icon={<FaChartBar />} title='Billing'/>
                 <Button title="Upgrade Now"/>
 
                 <MenuItem icon={<FaPowerOff />} title='Log Out'/>
