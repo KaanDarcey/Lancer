123a0dd09772a31dfae01ece46631cbe61e5e243
KaanDarcey <kaanhdarcey@gmail.com>
2020-02-13 19:27:50 -0800

    default pop up created

---
+++ src/stories/PopUp.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PopUp from '../comps/PopUp';
+
+export default {
+    title: 'PopUp',
+    component: PopUp,
+  };
+
+  export const defaultPopUp = () => {
+    return <PopUp />
+  };
