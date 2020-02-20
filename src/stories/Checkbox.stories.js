64e623d1c65824a6891855836795e69db7006a66
Karen <karencumlat@gmail.com>
2020-02-20 15:39:52 -0800

    Build Checkbox Comps
    #Create and style checkbox comps

---
+++ src/stories/Checkbox.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Checkbox from '../comps/Checkbox';
+
+export default {
+    title: 'Checkbox',
+    component: Checkbox,
+  };
+
+  export const defaultCheckbox = () => {
+    return <Checkbox />
+  };
