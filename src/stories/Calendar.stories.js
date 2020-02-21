f88397347a985f007bc786c333355021a351fd9c
KaanDarcey <kaanhdarcey@gmail.com>
2020-02-21 13:29:39 -0800

    schedule page and calendar comp created

---
+++ src/stories/Calendar.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calendar from '../comps/Calendar';
+
+export default {
+    title: 'Calendar',
+    component: Calendar,
+  };
+
+  export const defaultCalendar = () => {
+    return <Calendar />
+  };
