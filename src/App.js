e8d4e628c703f4d3b2cd383a2a47b55d6c43b201
KaanDarcey <kaanhdarcey@gmail.com>
2020-02-21 13:28:59 -0800

    imported additional screens

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Menu from './comps/Menu';
 import TaskPage from './screens/TaskPage';
 import ProjectPage from './screens/ProjectPage';
 import ClientPage from './screens/ClientPage';
+import SchedulePage from './screens/SchedulePage';
+import AddTaskPage from './screens/AddTaskPage';
+import ClientOverviewPage from './screens/ClientOverviewPage';
 
 function App() {
 
-  const [currentPage, setCurrentPage] = useState('');
+  const [currentPage, setCurrentPage] = useState('ClientOverview');
 
-  var page = <TaskPage />;
+  var page = <ClientPage />;
 
-  if(currentPage === 'Dashboard'){
-    // page = <DashboardPage />
-  } else
+  // if(currentPage === 'Dashboard'){
+  //   // page = <DashboardPage />
+  // } else
   if(currentPage === 'Schedule'){
-    // page = <SchedulePage />
+    page = <SchedulePage />
   } else
   if(currentPage === 'Projects'){
     page = <ProjectPage />
@@ -25,6 +28,12 @@ function App() {
   } else
   if(currentPage === 'ClientPage'){
     page = <ClientPage />
+  } else
+  if(currentPage === 'AddTask'){
+    page = <AddTaskPage />
+  } else
+  if(currentPage === 'ClientOverview'){
+    page = <ClientOverviewPage />
   }
 
   return (
