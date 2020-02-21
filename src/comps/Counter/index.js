834dab93d08664d45782fc9baf4410419e13eb55
KaanDarcey <kaanhdarcey@gmail.com>
2020-02-21 13:29:53 -0800

    counter edited

--- src/comps/Counter/index.js
+++ src/comps/Counter/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './counter.scss';
 
-function Counter({value, title}){
+function Counter({value, title, clr}){
     return(
         <div className='counter-container'>
-            <h3 className='counter-title'>{title}</h3>
-            <h2 className='counter-value'>{value}</h2>
+            <h3 className='counter-title' style={{color:clr}}>{title}</h3>
+            <h2 className='counter-value' style={{color:clr}}>{value}</h2>
         </div>
     )
 }
 
 Counter.defaultProps = {
     title:'default',
-    value:0
+    value:0,
+    clr:'#284b63'
 }
 
 export default Counter;
